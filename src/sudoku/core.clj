d8e26ed840d6cab04adc53cff1a22c71e79ce4af
Bobby Eickhoff <bobby.eickhoff2@kroger.com>
2013-12-02 12:51:32 -0500

    An hour-and-a-half later

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -1,7 +1,26 @@
 (ns sudoku.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn top-of-column [cell]
+  (mod cell 9))
+
+(defn head-of-row [cell]
+  (* (quot cell 9) 9)
+)
+
+(defn column-peer-cells [cell]
+  (range (top-of-column cell) 81 9))
+
+(defn row-peer-cells [cell]
+  (let [head (head-of-row cell)]
+    (range head (+ head 9))))
+
+(defn region-anchor [cell]
+  (let [x (quot (top-of-column cell) 3)
+        y (quot (head-of-row cell) 27)]
+    (+ (* 3 x) (* 27 y))))
+
+(defn region-peer-cells [cell]
+  (let [anchor (region-anchor cell)]
+    (for [y (range 0 3) x (range 0 3)]
+      (+ anchor x (* 9 y)))))
--- test/sudoku/core_test.clj
+++ test/sudoku/core_test.clj
@@ -2,6 +2,35 @@
   (:require [clojure.test :refer :all]
             [sudoku.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+; 0   1   2   3   4   5   6   7   8
+; 9   10  11  12  13...
+; 18  19  20  21  22
+; 27  28  29  30
+; ...
+
+(deftest test-geometry
+  (testing "top-of-column"
+    (is (= (top-of-column 10) 1))
+    (is (= (top-of-column 11) 2))
+  )
+  (testing "head-of-row"
+    (is (= (head-of-row 10) 9))
+    (is (= (head-of-row 5) 0))
+    (is (= (head-of-row 80) 72))
+  )
+  (testing "find column peer cells"
+    (is (= (column-peer-cells 1)  [1, 10, 19, 28, 37, 46, 55, 64, 73]))
+    (is (= (column-peer-cells 27) [0, 9, 18, 27, 36, 45, 54, 63, 72]))
+  )
+  (testing "find row peer cells"
+    (is (= (row-peer-cells 7)  [0, 1, 2, 3, 4, 5, 6, 7, 8]))
+    (is (= (row-peer-cells 27) [27, 28, 29, 30, 31, 32, 33, 34, 35]))
+  )
+  (testing "find region anchor"
+    (is (= (region-anchor 11) 0))
+    (is (= (region-anchor 13) 3))
+    (is (= (region-anchor 80) 60)))
+  (testing "find region peer cells"
+    (is (= (region-peer-cells 11) [0 1 2 9 10 11 18 19 20]))
+    (is (= (region-peer-cells 50) [30 31 32 39 40 41 48 49 50]))))
+
